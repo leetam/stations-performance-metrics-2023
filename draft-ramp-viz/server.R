c4bc831c664d62b31559a7750eb533c41ab616f6
leetam <leetam@pdx.edu>
2023-11-15 00:13:13 -0800

    added group configuration

--- draft-ramp-viz/server.R
+++ draft-ramp-viz/server.R
@@ -10,7 +10,7 @@ ramp_data <- readRDS("data/ramp.rds")
 
 function(input, output, session) {
   
-  ramp_data_filters <- reactive({
+  ramp_data_range_filters <- reactive({
     req(input$ramp_daterange,
         input$ramp_dow,
         input$ramp_timerange,
@@ -26,15 +26,31 @@ function(input, output, session) {
              resolution == input$ramp_resolution)
   })
   
+  ramp_data_configurations <- reactive({
+    req(ramp_data_range_filters())
+    ramp_data_range_filters() %>%
+      {if(input$ramp_group == "Yes") group_by(., time) else .} %>%
+      summarise(mean_volume = mean(metered_lane_volume))
+  })
+  
   output$ramp_volume_figure <- renderPlotly({
-    req(ramp_data_filters())
-    ramp_fig <- ramp_data_filters() %>%
-      ggplot(aes(x = start_time, y = metered_lane_volume)) +
-      geom_bar(stat = "identity", fill = "purple") +
-      theme_bw() +
-      xlab(NULL) +
-      ylab("Metered Lane Volume") +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    {if(input$ramp_group == "No")
+      ramp_data_range_filters() %>%
+        ggplot(aes(x = start_time, y = metered_lane_volume)) +
+        geom_bar(stat = "identity", fill = "purple") +
+        theme_bw() +
+        xlab(NULL) +
+        ylab("Metered Lane Volume") +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+      else
+        ramp_data_configurations() %>%
+        ggplot(aes(x = time, y = mean_volume)) +
+        geom_bar(stat = "identity", fill = "blue") +
+        theme_bw() +
+        xlab(NULL) +
+        ylab("Mean Metered Lane Volume") +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    }
   })
     
 
