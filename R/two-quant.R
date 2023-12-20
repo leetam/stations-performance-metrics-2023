d276238aa609e4be094a83c69ce6c737b3f070aa
leetam <leetam@pdx.edu>
2023-12-20 10:56:21 -0800

    worked out 2quant chart

--- R/two-quant.R
+++ R/two-quant.R
@@ -23,11 +23,36 @@ onequant <- highchart(type = "chart") %>%
   hc_add_series(
     subdatax[, "volume"],
     type = "column",
-    yAxis = 0
+    yAxis = 0,
+    name = "Volume"
     ) %>%
   hc_add_series(
     type = "line",
     subdatax[, "speed"],
-    yAxis = 1
-  )
+    yAxis = 1,
+    name = "Speed"
+  ) %>%
+  hc_xAxis(dateTimeLabelFormats = list(day = "%m %Y"), type = "datetime")
 onequant
+
+# onequant2 <- highchart(type = "chart") %>%
+#   hc_yAxis_multiples(
+#     list(lineWidth = 2,
+#          opposite = FALSE),
+#     list(showLastLabel = FALSE,
+#          opposite = TRUE)
+#   ) %>%
+#   hc_add_series(
+#     subdata,
+#     hcaes(x = starttime, y = volume),
+#     type = "column",
+#     yAxis = 0
+#   ) %>%
+#   hc_add_series(
+#     type = "line",
+#     subdata,
+#     hcaes(x = starttime, y = speed),
+#     yAxis = 1
+#   ) %>%
+#   hc_xAxis(dateTimeLabelFormats = list(day = "%m %Y"), type = "datetime")
+# onequant2
